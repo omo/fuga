9a2d9fdd5398d3a1e789dea259b0c598a2f84889
Hajime Morrita <omo@dodgson.org>
2014-01-05 17:28:44 +0900

    list: add --number flag.

--- list.go
+++ list.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	. "github.com/omo/fuga/base"
+	"math"
 	"os"
 	"path/filepath"
 	"regexp"
 	"sort"
+	"strconv"
 )
 
 var _ = fmt.Printf
@@ -60,9 +63,45 @@ func listPrimaryFiles(workspace string) []ListEntry {
 	return ret
 }
 
+func exp10(num int) int {
+	ret := 0
+	for 0 < num {
+		ret = ret + 1
+		num = num / 10
+	}
+
+	return ret
+}
+
+func imin(x, y int) int {
+	return int(math.Min(float64(x), float64(y)))
+}
+
 func (self *ListCommand) Run(args []string, settings CommandSettings) error {
-	for _, e := range listPrimaryFiles(settings.Workspace) {
-		fmt.Printf("%s\n", e.PrimaryFile)
+
+	count := 0
+	if 0 < len(args) {
+		n, err := strconv.ParseUint(args[0], 10, 32)
+		if nil != err {
+			fmt.Fprintf(os.Stderr, "%s\n", err.Error())
+			return nil
+		}
+
+		count = int(n)
+	}
+
+	list := listPrimaryFiles(settings.Workspace)
+	if count != 0 {
+		list = list[0:imin(count, len(list))]
+	}
+
+	width := exp10(len(list))
+	for i, e := range list {
+		if *shouldPrintOridinalNumber {
+			fmt.Printf("%"+strconv.FormatInt(int64(width), 10)+"d: %s\n", i, e.PrimaryFile)
+		} else {
+			fmt.Printf("%s\n", e.PrimaryFile)
+		}
 	}
 
 	return nil
@@ -71,6 +110,9 @@ func (self *ListCommand) Run(args []string, settings CommandSettings) error {
 func (self *ListCommand) Name() string {
 	return "list"
 }
+
+var shouldPrintOridinalNumber = flag.Bool("number", false,
+	"list: Print ordinal numbers for each item")
 
 func init() {
 	AddCommand(&ListCommand{})
